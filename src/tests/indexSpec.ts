5d079facb210ab52af0a361d9b2ba6dcd0e4041b
odeh sbeih <osbeih584@gmail.com>
2026-02-28 00:37:06 +0200

    add tests

--- src/tests/indexSpec.ts
+++ src/tests/indexSpec.ts
@@ -10,13 +10,17 @@ describe("Test endpoint response", () => {
     const response = await request.get("/api");
     expect(response.status).toBe(200);
   });
+  it("should return 404 invalid endpoint", async () => {
+    const response = await request.get("/apis");
+    expect(response.status).toBe(404);
+  });
 });
 
 describe("Test Image endpoint", () => {
+  const fileName = "fjord";
+  const width = 200;
+  const height = 200;
   it("gets the Image endpoint", async () => {
-    const fileName = "fjord";
-    const width = 200;
-    const height = 200;
     const response = await request.get(
       `/api/image?fileName=${fileName}&width=${width}&height=${height}`,
     );
@@ -25,11 +29,23 @@ describe("Test Image endpoint", () => {
     }
     expect(response.status).toBe(200);
   });
+  it("invalid end point", async () => {
+    const response = await request.get(
+      `/api/img?fileName=${fileName}&width=${width}&height=${height}`,
+    );
+    expect(response.status).toBe(404);
+  });
 });
 
 describe("test process image", () => {
   it("resize the image", async () => {
     const outputPath = await processImage("fjord", 200, 200);
     expect(fs.existsSync(outputPath)).toBeTrue();
+  });
+  it("should return error: file not exist", async () => {
+    await expectAsync(processImage("notexist", 200, 200)).toBeRejected();
+  });
+  it("should return error: width or height invalid", async () => {
+    await expectAsync(processImage("notexist", 200, -200)).toBeRejected();
   });
 });
--- src/utilities/processImage.ts
+++ src/utilities/processImage.ts
@@ -9,6 +9,9 @@ const processImage = async (
   const imagePath = path.join(process.cwd(), "images", `${fileName}.jpg`);
   const outputPath = path.join(process.cwd(), "thumb", `${fileName}.thumb.jpg`);
 
+  if (width < 1 || height < 1) {
+    throw new Error("width or height invalid");
+  }
   await sharp(imagePath).resize(width, height).toFile(outputPath);
 
   return outputPath;
